d02e5c073fb73de083d2a2b6271b828fabb6a2fc
KingKara <kinga.kara.93@gmail.com>
2019-09-21 21:00:06 +0200

    Task 29.4

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -25,20 +25,20 @@ public class EmailScheduler {
     private static final String MANYTASKS = " tasks.";
 
 //    @Scheduled(cron="0 0 10 * * *")
-    @Scheduled(fixedDelay = 10000)
-    public void sendInformationEmail() {
-        long size = taskRepository.count();
-        String messageLastPart;
-        if (size==(long)1) {
-            messageLastPart = ONETASK;
-        } else {
-            messageLastPart = MANYTASKS;
-        }
-        simpleEmailService.send(new Mail(
-                adminConfig.getAdminMail(),
-                SUBJECT,
-                "Currently in database you got: " + size + messageLastPart,
-                null
-        ));
-    }
+//    @Scheduled(fixedDelay = 10000)
+//    public void sendInformationEmail() {
+//        long size = taskRepository.count();
+//        String messageLastPart;
+//        if (size==(long)1) {
+//            messageLastPart = ONETASK;
+//        } else {
+//            messageLastPart = MANYTASKS;
+//        }
+//        simpleEmailService.send(new Mail(
+//                adminConfig.getAdminMail(),
+//                SUBJECT,
+//                "Currently in database you got: " + size + messageLastPart,
+//                null
+//        ));
+//    }
 }
